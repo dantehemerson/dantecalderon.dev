aa1e39d4089211a3a68003680e9d746b81a4b152
Dante Calderón <dantehemerson@gmail.com>
2018-06-13 05:24:53 -0500

    Resolving path in css

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Rotational from '../components/Rotational'
 class Index extends React.Component {
 	render() {
 		return (
-			<main className="Header-Home">
+			<main className="Header-Home">			
 				<h1 className="Header-Home__title">Dante Calderón</h1>
 				<h2 className="Header-Home__subtitle">Javascript Developer</h2>
 				<Rotational/>

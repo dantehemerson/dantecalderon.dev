35a9ba4059e7cdc0a620baf0f6ccbb68a7d1a635
Dante Calderón <dantehemerson@gmail.com>
2018-06-13 05:46:06 -0500

    Funcion for add items in Rotational

--- src/components/Rotational/Rotational.js
+++ src/components/Rotational/Rotational.js
@@ -4,6 +4,9 @@ const Rotational = (props) => (
 	<div className="Rotational">
 		<div className="Rotational__orbit">
 			<div className="Rotational__item computer"></div>
+			<div className="Rotational__item hacker"></div>
+			<div className="Rotational__item server"></div>
+			<div className="Rotational__item www"></div>
 		</div>
 	</div>
 )

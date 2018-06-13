e178f99db1de8a51c988c75fb81652cf59b85057
Dante Calderón <dantehemerson@gmail.com>
2018-06-13 06:44:47 -0500

    Adding more items in Rotational

--- src/components/Rotational/Rotational.js
+++ src/components/Rotational/Rotational.js
@@ -7,6 +7,10 @@ const Rotational = (props) => (
 			<div className="Rotational__item hacker"></div>
 			<div className="Rotational__item server"></div>
 			<div className="Rotational__item www"></div>
+			<div className="Rotational__item browser"></div>
+			<div className="Rotational__item protection"></div>
+			<div className="Rotational__item laptop"></div>
+			<div className="Rotational__item headphones"></div>
 		</div>
 	</div>
 )

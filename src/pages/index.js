2fc74edbb9bb52f21163fafd549d391bf3288dac
Dante Calderón <dantehemerson@gmail.com>
2018-06-13 03:11:49 -0500

    Styles...

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,10 @@ import React from 'react'
 class Index extends React.Component {
 	render() {
 		return (
-			<h1>Inicio</h1>
+			<main className="Header-Home">
+				<h1 className="Header-Home__title">Dante Calderón</h1>
+				<h2 className="Header-Home__subtitle">Javascript Developer</h2>
+			</main>
 		)
 	}
 }

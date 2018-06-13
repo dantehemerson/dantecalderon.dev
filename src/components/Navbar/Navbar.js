8bd51fb71ab63d884362f28bd0951d6c4aa6fd59
Dante Calderón <dantehemerson@gmail.com>
2018-06-13 02:46:42 -0500

    Styles navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -22,13 +22,13 @@ class Navbar extends React.Component {
 						<li className={(this.props.activePage === '' ? 'Navbar__item' : '')}>
 							<Link className="Navbar__link" to="/">Inicio</Link>
 						</li>
-						<li className={(this.props.activePage === 'Blog' ? 'Navbar__item' : '')}>
+						<li className={(this.props.activePage === 'Blog' ? 'Navbar__item' : 'Navbar__item')}>
 							<Link className="Navbar__link" to="/blog">Blog</Link>
 						</li>
-						<li className={(this.props.activePage === 'Contact' ? 'Navbar__item' : '')}>
+						<li className={(this.props.activePage === 'Contact' ? 'Navbar__item' : 'Navbar__item')}>
 							<Link className="Navbar__link" to="/contact">Contacto</Link>
 						</li>						
-						<li className={(this.props.activePage === 'Portfolio' ? 'Navbar__item' : '')}>
+						<li className={(this.props.activePage === 'Portfolio' ? 'Navbar__item' : 'Navbar__item')}>
 							<Link className="Navbar__link" to="/portfolio">Portafolio</Link>
 						</li>						
 					</ul>					

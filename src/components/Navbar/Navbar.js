1231c7dd30989cdd2d0b036a5ec5b9552e19a32c
Dante Calderón <dantehemerson@gmail.com>
2018-06-12 23:57:55 -0500

    ...

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -15,8 +15,8 @@ class Navbar extends Component {
 	render() {		
 		return (
 			<nav className="Navbar">				
-				<div className="Navbar__title">
-					<Link to="/">Dante Calderon</Link>
+				<div className="Navbar__title-container">
+					<Link className="Navbar__title" to="/">Dante Calderon</Link>
 				</div>
 				<div className="links">
 					<ul>

c3125d94b88743bcb0332648fb48136a87a7768f
Dante Calderón <dantehemerson@gmail.com>
2018-06-12 23:11:10 -0500

    Configuring typography

---
+++ src/utils/typography.js
@@ -0,0 +1,8 @@
+import Typography from 'typography'
+
+const typography = new Typography({
+	headerFontFamily: ['Open Sans', 'sans-serif'],
+	bodyFontFamily: ['Open Sans', 'sans-serif'],
+})
+
+export default typography

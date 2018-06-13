393b5fad8e34c454807b5b61bc785484a8a24326
Dante Calderón <dantehemerson@gmail.com>
2018-06-12 23:05:48 -0500

    Configuring plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,24 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+  	'gatsby-plugin-react-helmet',
+		'gatsby-plugin-sass',
+		{
+			resolve: 'gatsby-plugin-typography',
+			options: {
+				pathToConfigModule: 'src/utils/typography.js',
+			}
+			
+		},
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/assets/images/`,
+      }
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+  ],
 }

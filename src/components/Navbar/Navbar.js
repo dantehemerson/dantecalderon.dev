c6d11d0b01c96757011229c1e4293038b952cc75
Dante Calderón <dantehemerson@gmail.com>
2018-06-12 23:51:06 -0500

    Changing name of clases

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -21,9 +21,9 @@ class Navbar extends Component {
 				<div className="links">
 					<ul>
 						<li className={(this.props.activePage === '' ? 'active' : '')}><Link to="/">Inicio</Link></li>
-						<li className={(this.props.activePage === 'Nosotros·' ? 'active' : '')}><Link to="/blog">Blog</Link></li>
-						<li className={(this.props.activePage === 'Contacto·' ? 'active' : '')}><Link to="/contact">Contacto</Link></li>						
-						<li className={(this.props.activePage === 'Contacto·' ? 'active' : '')}><Link to="/portfolio">Portafolio</Link></li>						
+						<li className={(this.props.activePage === 'Blog' ? 'active' : '')}><Link to="/blog">Blog</Link></li>
+						<li className={(this.props.activePage === 'Contact' ? 'active' : '')}><Link to="/contact">Contacto</Link></li>						
+						<li className={(this.props.activePage === 'Portfolio' ? 'active' : '')}><Link to="/portfolio">Portafolio</Link></li>						
 					</ul>					
 				</div>
 			</nav>

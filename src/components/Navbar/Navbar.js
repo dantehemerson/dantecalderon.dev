cb8031a604e427a661da9b91367fef4c9a87c38b
Dante Calderón <dantehemerson@gmail.com>
2018-06-12 23:45:12 -0500

    Creating Navbar component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import Link from 'gatsby-link'
+
+
+
+class Navbar extends Component {	
+	static propTypes = {
+		activePage: PropTypes.string,
+	}
+
+
+
+
+	render() {		
+		return (
+			<nav className="navbar">				
+				<div className="app-name">
+					<Link to="/">Lotcod</Link>
+				</div>
+				<div className="links">
+					<ul>
+						<li className={(this.props.activePage === '' ? 'active' : '')}><Link to="/">Inicio</Link></li>
+						<li className={(this.props.activePage === 'Nosotros·' ? 'active' : '')}><Link to="/nosotros">Nosotros</Link></li>
+						<li className={(this.props.activePage === 'Contacto·' ? 'active' : '')}><Link to="/contacto">Contacto</Link></li>						
+					</ul>					
+				</div>
+			</nav>
+		)
+	}
+}
+
+export default Navbar
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+class Index extends React.Component {
 
-export default IndexPage
+	render() {
+		return (
+			<h1>Inicio</h1>
+		)
+	}
+}
+
+export default Index

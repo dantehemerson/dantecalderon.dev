6b2d181bf211b8d5eee1574603e5ae61046013d8
Dante Calderón <dantehemerson@gmail.com>
2018-06-14 17:57:03 -0500

    Some changes

---
+++ src/pages/contact.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+class Contact extends React.Component {
+	render() {
+		return (
+			<div>
+				<section className="Page">
+					<div className="container">
+						<div className="row center-xs">
+							<div className="About__titlewrap Page__titlewrap text-center col-xs-12 col-md-10">
+								<h2 className="About__title Page__title">Sobre mí</h2>
+								<p className="About__description Page__description">¡Hola! :smiley: Soy Dante Calderon. Soy Programador en Javascript, Go & Python</p>
+							</div>
+						</div>
+					</div>
+				</section>
+				
+			</div>
+		)
+	}
+}
+
+export default Contact	

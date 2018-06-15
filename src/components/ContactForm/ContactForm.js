062b9d4a3692ad95f68bccd28e56fa13fbdd39bc
Dante Calderón <dantehemerson@gmail.com>
2018-06-14 19:23:18 -0500

    Styling inputs

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -4,44 +4,44 @@ class ContactForm extends React.Component {
 
 	render() {
 		return (
-			<div className="form-container">
+			<div className="ContactForm">
 				<form>
-					<div className="label-input">
-						<div className="title-input">
-							<p>Tu nombre</p>
+					<div className="ContactForm__item">
+						<div className="ContactForm__labelwrap">
+							<p className="ContactForm__label">Tu nombre</p>
 						</div>
-						<div className="input-container">
-							<input className="input" placeholder="Nombre" type="text"/>
+						<div className="ContactForm__inputwrap">
+							<input className="ContactForm__input" placeholder="Nombre" type="text"/>
 						</div>
 					</div>		
-					<div className="label-input">
-						<div className="title-input">
-							<p>Tu compañia</p>
+					<div className="ContactForm__item">
+						<div className="ContactForm__labelwrap">
+							<p className="ContactForm__label">Tu correo</p>
 						</div>
-						<div className="input-container">
-							<input className="input" placeholder="Nombre de la compañia" type="text"/>
-						</div>
-					</div>
-					<div className="label-input">
-						<div className="title-input">
-							<p>Tu correo</p>
-						</div>
-						<div className="input-container">
-							<input className="input" placeholder="E-mail" type="text"/>
+						<div className="ContactForm__inputwrap">
+							<input className="ContactForm__input" placeholder="E-mail" type="text"/>
 						</div>
 					</div>
-					<div className="label-input label-describe">
-						<div className="title-input">
-							<p>Describenos lo que tu necesitas, mientras mas conoscamos, mejor.</p>
+					<div className="ContactForm__item">
+						<div className="ContactForm__labelwrap">
+							<p className="ContactForm__label">Asunto</p>
 						</div>
-						<div className="input-container">
-							<textarea className="input" placeholder="Describe" rows="9"></textarea>
+						<div className="ContactForm__inputwrap">
+							<input className="ContactForm__input" placeholder="Asunto" type="text"/>
+						</div>
+					</div>
+					<div className="ContactForm__item">
+						<div className="ContactForm__labelwrap">
+							<p className="ContactForm__label">Describenos lo que tu necesitas, mientras mas conoscamos, mejor.</p>
+						</div>
+						<div className="ContactForm__inputwrap">
+							<textarea className="ContactForm__input" placeholder="Describe" rows="6"></textarea>
 						</div>
 					</div>
 
-					<div className="label-input">
-						<div className="submit-container">
-							<input className="btn" placeholder="E-mail" type="submit"/>
+					<div className="ContactForm__item">
+						<div className="ContactForm__submitwrap">
+							<input className="ContactForm__submit" placeholder="E-mail" type="submit"/>
 						</div>
 					</div>
 				</form>

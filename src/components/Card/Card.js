268e60ca9a231676723b65fe1bcb0d53e51b98f9
Dante Calderón <dantehemerson@gmail.com>
2018-06-15 22:09:31 -0500

    ...

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import Img from 'gatsby-image'
 
 class Card extends React.Component {
 	render() {
 		return (
 			<div className="Card">
 				<div className="Card__header">
+
 				</div>
 				<div className="Card__body">
-					<h3 className="Card__title"><h3>
+					<h3 className="Card__title"></h3>
 					<p className="Card__content"></p>
 					<p><time datetime="2008-02-14 20:00">24 Junio</time></p>
 				</div>

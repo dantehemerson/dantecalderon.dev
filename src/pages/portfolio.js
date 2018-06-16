1a7dda5f05c6159583fbda0cefb80e25cdb3d0fa
Dante Calderón <dantehemerson@gmail.com>
2018-06-15 21:25:47 -0500

    Portfolio page

---
+++ src/pages/portfolio.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+class Blog extends React.Component {
+	render() {
+		const { data } = this.props  	
+		return (
+			<div className="Contact">
+				<section className="Contact Page">
+					<div className="container">
+						<div className="row center-xs">
+							<div className="Contact__titlewrap Page__titlewrap text-center col-xs-12 col-md-10 col-lg-7">
+								<h2 className="Contact__title Page__title">Portfolio</h2>
+								<p className="Contact__description Page__description">Sobre programacion y mas.</p>
+							</div>							
+						</div>
+					</div>					
+				</section>
+				<div className="Contact__form container--full">
+					<div className="row">
+						{/*items here*/}
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
+
+export const queryPortfolio = graphql`
+  query QueryPortfolio {
+    contactImage: imageSharp(id: {regex: "/contact/"}) {
+      sizes(maxWidth: 4000) {
+        ...GatsbyImageSharpSizes_tracedSVG
+      }
+    }     
+    site {
+      siteMetadata {
+            title
+      }     
+    }
+  }
+`
+
+export default Blog	

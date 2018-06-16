76a9e618b06cb849bead4fe94ab4d96b9b5ebfa2
Dante Calderón <dantehemerson@gmail.com>
2018-06-15 22:58:15 -0500

    Card styles

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -11,8 +11,8 @@ class Card extends React.Component {
 				<div className="Card__body">
 					<h3 className="Card__title">{ this.props.data.title }</h3>
 					<p className="Card__content">{ this.props.data.content }</p>
-					<p><time datetime="2008-02-14 20:00">{ this.props.data.date}</time></p>
-				</div>
+					<p className="Card__date"><time datetime="2008-02-14 20:00">{ this.props.data.date}</time></p>
+				</div>				
 			</div>
 		)
 	}
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,24 +1,41 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import Card from '../components/Card'
+
 class Blog extends React.Component {
 	render() {
 		const { data } = this.props  	
+		const post = {
+			image: data.contactImage,
+			title: 'Mi nueva vida',
+			content: 'Este es mi nuev pagina web, ya que la anterior estaba muy antigua. Pero como vamos a empezar a desarrollar un nuevo tipo de publiciones',
+			date: '20 Junio 2018'
+
+		}
 		return (
-			<div className="Contact">
-				<section className="Contact Page">
+			<div className="Blog">
+				<section className="HeaderBlog Page">
 					<div className="container">
 						<div className="row center-xs">
-							<div className="Contact__titlewrap Page__titlewrap text-center col-xs-12 col-md-10 col-lg-7">
-								<h2 className="Contact__title Page__title">Blog</h2>
-								<p className="Contact__description Page__description">Sobre programacion y mas.</p>
+							<div className="HeaderBlog__titlewrap Page__titlewrap text-center col-xs-12 col-md-10 col-lg-7">
+								<h2 className="HeaderBlog__title Page__title">Blog</h2>
+								<p className="HeaderBlog__description Page__description">Sobre programacion y mas.</p>
 							</div>							
 						</div>
 					</div>					
 				</section>
 				<div className="Contact__form container--full">
 					<div className="row">
-						{/*items here*/}
+						<div className="col-xs-4">
+							<Card data={post} />
+						</div>
+						<div className="col-xs-4">
+							<Card data={post} />
+						</div>
+						<div className="col-xs-4">
+							<Card data={post} />
+						</div>
 					</div>
 				</div>
 			</div>

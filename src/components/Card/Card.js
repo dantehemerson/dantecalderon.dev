1d108cb797dbfb6f564d333282f546efc6cf861d
Dante Calderón <dantehemerson@gmail.com>
2018-06-15 22:24:48 -0500

    ..

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,12 +6,12 @@ class Card extends React.Component {
 		return (
 			<div className="Card">
 				<div className="Card__header">
-
+					<Img sizes={this.props.data.image.sizes}/>
 				</div>
 				<div className="Card__body">
-					<h3 className="Card__title"></h3>
-					<p className="Card__content"></p>
-					<p><time datetime="2008-02-14 20:00">24 Junio</time></p>
+					<h3 className="Card__title">{ this.props.data.title }</h3>
+					<p className="Card__content">{ this.props.data.content }</p>
+					<p><time datetime="2008-02-14 20:00">{ this.props.data.date}</time></p>
 				</div>
 			</div>
 		)

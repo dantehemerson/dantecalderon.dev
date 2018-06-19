f8d11b1cf51bc35342b8ff8cf4e4eca4802ee203
Dante Calderón <dantehemerson@gmail.com>
2018-06-18 21:18:10 -0500

    ...

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -13,8 +13,8 @@ class Card extends React.Component {
 					</div>
 					<div className="Card__body">
 						<h3 className="Card__title">{ this.props.data.title }</h3>
-						<p className="Card__content">{ this.props.data.content }</p>
 						<p className="Card__date"><time datetime="2008-02-14 20:00">{ this.props.data.date}</time></p>
+						<p className="Card__content">{ this.props.data.content }</p>
 					</div>		
 				</Link>		
 			</div>

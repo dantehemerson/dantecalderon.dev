59c7dcc92b2c9eb16836e8f72ff1b7b720ee33be
Dante Calderón <dantehemerson@gmail.com>
2018-07-20 20:41:46 -0500

    status

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,23 +6,21 @@ class Card extends React.Component {
 	render() {
 		const blogMode = this.props.mode === 'blog'
 		return (
-			<div  className={`Card ${(blogMode ? 'blogMode' : '')}`}>
-				<Link to="/">
-					<div className="Card__header">
-						<Img sizes={this.props.data.image.sizes}/>
-					</div>
-					<div className="Card__body">
-						<h3 className="Card__title">{ this.props.data.title }</h3>
-						<p className="Card__date"><time dateTime="2008-02-14 20:00">{ this.props.data.date}</time></p>
-						<p className="Card__content">{ this.props.data.content }</p>						
-						<Link className="btn Card__btn" to="/blog">Seguir Leyendo ➞</Link>
-						<div className="Card__wavey"></div>
-						{/*<div className="Card__tags">
-							<p className="Card__tag">Tag</p>
-						</div>						
-						*/}
-					</div>		
-				</Link>		
+			<div  className={`Card ${(blogMode ? 'blogMode' : '')}`}>				
+				<div className="Card__header">
+					<Img sizes={this.props.data.image.sizes}/>
+				</div>				
+				<div className="Card__body">
+					<h3 className="Card__title">{ this.props.data.title }</h3>
+					<p className="Card__date"><time dateTime="2008-02-14 20:00">{ this.props.data.date}</time></p>
+					<p className="Card__content">{ this.props.data.content }</p>						
+					<Link className="btn Card__btn" to="/blog">Seguir Leyendo ➞</Link>
+					<div className="Card__wavey"></div>
+					{/*<div className="Card__tags">
+						<p className="Card__tag">Tag</p>
+					</div>						
+					*/}
+				</div>		
 			</div>
 		)
 	}

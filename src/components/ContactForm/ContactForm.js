d71acd1f9b5cc23186411a39a96a4d55829755fa
Dante Calderón <dantehemerson@gmail.com>
2018-07-25 09:25:38 -0500

    clases for netlify forms

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -5,13 +5,13 @@ class ContactForm extends React.Component {
 	render() {
 		return (
 			<div className="ContactForm">
-				<form>
+				<form name="contact" method="POST" netlify="true">
 					<div className="ContactForm__item">
 						<div className="ContactForm__labelwrap">
 							<p className="ContactForm__label">Tu nombre</p>
 						</div>
 						<div className="ContactForm__inputwrap">
-							<input className="ContactForm__input" name="name" placeholder="Nombre" type="text"/>
+							<input name="name" className="ContactForm__input" placeholder="Nombre" type="text"/>
 						</div>
 					</div>		
 					<div className="ContactForm__item">
@@ -19,7 +19,7 @@ class ContactForm extends React.Component {
 							<p className="ContactForm__label">E-mail</p>
 						</div>
 						<div className="ContactForm__inputwrap">
-							<input className="ContactForm__input" placeholder="tu@correo.com" type="text"/>
+							<input name="email" className="ContactForm__input" placeholder="tu@correo.com" type="text"/>
 						</div>
 					</div>
 					<div className="ContactForm__item">
@@ -27,7 +27,7 @@ class ContactForm extends React.Component {
 							<p className="ContactForm__label">Asunto</p>
 						</div>
 						<div className="ContactForm__inputwrap">
-							<input className="ContactForm__input" placeholder="Asunto" type="text"/>
+							<input name="subject" className="ContactForm__input" placeholder="Asunto" type="text"/>
 						</div>
 					</div>
 					<div className="ContactForm__item">
@@ -35,9 +35,10 @@ class ContactForm extends React.Component {
 							<p className="ContactForm__label">Tu mensaje</p>
 						</div>
 						<div className="ContactForm__inputwrap">
-							<textarea className="ContactForm__input" placeholder="Ingresa tu mensaje aquí" rows="6"></textarea>
+							<textarea name="message" className="ContactForm__input" placeholder="Ingresa tu mensaje aquí" rows="6"></textarea>
 						</div>
 					</div>
+					<div data-netlify-recaptcha></div>
 					<div className="ContactForm__item">
 						<div className="ContactForm__submitwrap">
 							<input className="ContactForm__submit btn" placeholder="E-mail" type="submit"/>

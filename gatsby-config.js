34377e8e65156dd322337128561bd981d679aa2e
Dante Calderón <dantehemerson@gmail.com>
2018-07-25 14:00:02 -0500

    Configuring remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,36 @@ module.exports = {
         path: `${__dirname}/src/assets/images/`,
       }
     },
+    { // Paginas no utilizado aún.
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: `${__dirname}/src/pages/`,
+      }
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+              sizeByPixelDensity: true,
+            },
+          },
+          {
+            resolve: `gatsby-remark-response-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+        ],
+      },
+    },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     'gatsby-plugin-netlify-cms',

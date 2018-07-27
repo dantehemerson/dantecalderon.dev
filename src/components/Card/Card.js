1241cf584948f017eb089c0d898599f46d9e68b8
Dante Calderón <dantehemerson@gmail.com>
2018-07-27 04:36:15 -0500

    Time in post

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -13,7 +13,7 @@ class Card extends React.Component {
 				</div>				
 				<div className="Card__body">
 					<Link to={ this.props.data.path }><h3 className="Card__title">{ this.props.data.title }</h3></Link>
-					<p className="Card__date"><time dateTime="2008-02-14 20:00">{ this.props.data.date}</time></p>
+					<p className="Card__date"><time dateTime="2008-02-14 20:00">{ this.props.data.date}</time> &middot; 🕛 { this.props.data.timeToRead} min read</p>
 					<p className="Card__content">{ this.props.data.excerpt }</p>						
 					<Link className="btn Card__btn" to={ this.props.data.path }>Seguir Leyendo ➞</Link>
 					{/*<div className="Card__tags">

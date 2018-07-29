0222cd7fc93c2b2467f974c979430d5bcdc5e121
Dante Calderón <dantehemerson@gmail.com>
2018-07-29 06:04:53 -0500

    Normal image and medium image

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -13,7 +13,7 @@ import SEO from '../components/SEO'
 export const Post = ({ content, frontmatter, previous, next, siteTitle, image, siteUrl, contentComponent }) => {
 	const PostContent = contentComponent || Content
 	return (
-		<div className="Post full-image">
+		<div className="Post normal-img">
 			<div className="Post__header">
 				<div className="Post__header__data">
 					<h1 className="Post__title">{ frontmatter.title }</h1>

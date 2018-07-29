8fe62b199e334805a448969f8468982e8b7701e4
Dante Calderón <dantehemerson@gmail.com>
2018-07-28 19:41:45 -0500

    Removing GA

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -22,20 +22,7 @@ class Layout extends React.Component {
       <div>
         <Helmet>
           <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700|Open+Sans:400,600,700" rel="stylesheet"/>
-          <meta name="theme-color" content="#DFBD83"/>
-          {
-            /**
-            * 
-            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-121858272-1"></script>
-            <script>
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', 'UA-121858272-1');
-            </script>
-            **/
-          }
+          <meta name="theme-color" content="#DFBD83"/>          
         </Helmet>
         <Navbar/>
         <div>

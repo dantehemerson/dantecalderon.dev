fad3f95212fe7b40b7f97250575c63009a6b4fa1
Dante Calderón <dantehemerson@gmail.com>
2018-07-29 19:48:38 -0500

    Form with state

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,23 +1,52 @@
 import React from 'react'
 
+
+const encode = (data) => {
+	return Object.keys(data)
+		.map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+		.join("&")
+}
+
 class ContactForm extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			name: "",
+			email: "",
+			subject: "",
+			message: ""
+		}
+	}
+
+	handleSubmit = e => {
+		fetch("/", {
+			method: "POST",
+			headers: {
+				 "Content-Type": "application/x-www-form-urlencoded"
+			},
+			body: encode({ "form-name": "contact", ...this.state })
+		})
+			.then(() => alert("Success!!!"))
+			.catch(error => alert("Error"))
+		e.preventDefault()
+	}
+
+	handleChange = e => this.setState({ [e.target.name]: e.target.value })
 
 	render() {
+		const { name, email, subject, message } = this.state
 		return (
 			<div className="ContactForm">
 				<form 
-					name="contact" 
-					method="POST"
-					data-netlify="true"
-					data-netlify-honeypot="bot-field"
+					name="contact"
+					onSubmit={ this.handleSubmit }
 					>
-					<input type="hidden" name="form-name" value="contact" />
 					<div className="ContactForm__item">
 						<div className="ContactForm__labelwrap">
 							<p className="ContactForm__label">Tu nombre</p>
 						</div>
 						<div className="ContactForm__inputwrap">
-							<input name="name" className="ContactForm__input" placeholder="Nombre" type="text"/>
+							<input name="name" value={name} className="ContactForm__input" placeholder="Nombre" type="text" required onChange={ this.handleChange }/>
 						</div>
 					</div>		
 					<div className="ContactForm__item">
@@ -25,7 +54,7 @@ class ContactForm extends React.Component {
 							<p className="ContactForm__label">E-mail</p>
 						</div>
 						<div className="ContactForm__inputwrap">
-							<input name="email" className="ContactForm__input" placeholder="tu@correo.com" type="text"/>
+							<input name="email" value={email} className="ContactForm__input" placeholder="tu@correo.com" type="email" required onChange={ this.handleChange }/>
 						</div>
 					</div>
 					<div className="ContactForm__item">
@@ -33,7 +62,7 @@ class ContactForm extends React.Component {
 							<p className="ContactForm__label">Asunto</p>
 						</div>
 						<div className="ContactForm__inputwrap">
-							<input name="subject" className="ContactForm__input" placeholder="Asunto" type="text"/>
+							<input name="subject" value={subject} className="ContactForm__input" placeholder="Asunto" type="text" required onChange={ this.handleChange }/>
 						</div>
 					</div>
 					<div className="ContactForm__item">
@@ -41,7 +70,7 @@ class ContactForm extends React.Component {
 							<p className="ContactForm__label">Tu mensaje</p>
 						</div>
 						<div className="ContactForm__inputwrap">
-							<textarea name="message" className="ContactForm__input" placeholder="Ingresa tu mensaje aquí" rows="6"></textarea>
+							<textarea name="message" value={message} className="ContactForm__input" placeholder="Ingresa tu mensaje aquí" rows="6" required onChange={ this.handleChange }></textarea>
 						</div>
 					</div>					
 					<div className="ContactForm__item">

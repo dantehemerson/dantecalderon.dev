b7d074f218a971edf6375b69a6b6f943f8b0adb0
Dante Calderón <dantehemerson@gmail.com>
2018-08-26 22:37:38 -0500

    Logo in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react'	
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Img from 'gatsby-image'
 
-import Logo from '../../assets/images/logo.jpg'
+import Logo from '../../assets/images/logo_letter.png'
 
 class Navbar extends React.Component {	
 	state = {

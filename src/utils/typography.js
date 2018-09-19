9e38b10a053682a45cc1f86e81cef723d90a7d74
dantehemerson <dantehemerson@gmail.com>
2018-09-19 00:11:42 -0500

    Migrate Typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,8 +1,11 @@
 import Typography from 'typography'
 
+
 const typography = new Typography({
 	headerFontFamily: ['Playfair Display', 'serif'],
 	bodyFontFamily: ['Open Sans', 'sans-serif']
 })
 
-export default typography
+const { rhythm, scale } = typography
+
+export { rhythm, scale, typography as default }

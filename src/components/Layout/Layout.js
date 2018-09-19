72e29ff831e45eedcfe52d67cae3ef00273ed22a
dantehemerson <dantehemerson@gmail.com>
2018-09-18 23:27:08 -0500

    :hammer: Remove layout folder, it is necessary for migration

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -13,24 +13,24 @@ import 'react-awesome-button/src/styles/styles.scss'
 import './index.scss'
 
 class Layout extends React.Component {
-   static propTypes = {
-      children: PropTypes.func,
-   }
+	static propTypes = {
+		children: PropTypes.func,
+	}
 
-   componentDidMount() {
-      require('@webcomponents/custom-elements/custom-elements.min.js')
-      require('@clr/icons/clr-icons.min.js')
-   }
+	componentDidMount() {
+		require('@webcomponents/custom-elements/custom-elements.min.js')
+		require('@clr/icons/clr-icons.min.js')
+	}
 
-   render() {
-      return (
-         <div>
-           	<Helmet>
-					<link 
-					href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800|Noto+Serif:400,700,400italic,700italic|Playfair+Display:400,700" rel="stylesheet"/>      
-					<meta name="theme-color" content="#DFBD83"/>          
+	render() {
+		return (
+			<div>
+				<Helmet>
+					<link
+					href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800|Noto+Serif:400,700,400italic,700italic|Playfair+Display:400,700" rel="stylesheet"/>
+					<meta name="theme-color" content="#DFBD83"/>
 					<meta name="distribution" content="global"/>
-					<meta httpEquiv="Content-Language" content="es" />					
+					<meta httpEquiv="Content-Language" content="es" />
 					<link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
 					<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
 					<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
@@ -43,19 +43,19 @@ class Layout extends React.Component {
 					<link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
 					<link rel="icon" type="image/png" sizes="32x32"	href="/favicon-32x32.png" />
 					<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
-					<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />         
+					<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 					<meta name="msapplication-TileColor" content="#DFBD83" />
 					<meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-           	</Helmet>
-				  
-           	<Navbar activePage={ actualPage(this.props.location.pathname) }/>
-           	<div className="Dyamic-container">
-            	{ this.props.children() }
-           	</div>
-           	<Footer/>
-         </div>
-      )
-   }
+				</Helmet>
+
+				<Navbar activePage={ actualPage(this.props.location.pathname) }/>
+				<div className="Dyamic-container">
+					{ this.props.children }
+				</div>
+				<Footer/>
+			</div>
+			)
+	}
 }
 
 export default Layout

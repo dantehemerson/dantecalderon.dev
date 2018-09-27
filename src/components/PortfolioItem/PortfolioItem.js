cb6f897bbe9a566ef440afd482fc716ad4c28dce
dantehemerson <dantehemerson@gmail.com>
2018-09-27 12:02:25 -0500

    Change tags icon to svg

--- src/components/PortfolioItem/PortfolioItem.js
+++ src/components/PortfolioItem/PortfolioItem.js
@@ -120,11 +120,15 @@ export default props => (
 			<Title>{ props.data.title }</Title>
 		</ItemWrapper>
 		<TagsWrapper className="PortfolioItem__tagswrapper">
+			<svg className="icon_svg" version="1.1" width={36} height={36} viewBox="0 0 36 36" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+			  <title>tags-line</title>
+			  <path d="M33.16,19.13,19.58,5.55A1.92,1.92,0,0,0,18.21,5H16.12L31.75,20.45,21.22,31.07a1.93,1.93,0,0,0,2.73,0l9.21-9.21a1.93,1.93,0,0,0,0-2.73Z" /><circle cx="7.81" cy="11.14" r="1.33" /><path d="M27.78,19.17,14.2,5.58A1.92,1.92,0,0,0,12.83,5H3.61A1.93,1.93,0,0,0,1.68,6.93v9.22a1.92,1.92,0,0,0,.57,1.36L15.84,31.1a1.93,1.93,0,0,0,2.73,0l9.21-9.21A1.93,1.93,0,0,0,27.78,19.17ZM17.26,29.69,3.69,16.15V7h9.1L26.37,20.48Z" />
+			  <rect x={0} y={0} width={36} height={36} fillOpacity={0} />
+			</svg>
 			{
 				props.data.tags.map((value, index) => {
 					return (
 						<Tag key={ index }>
-							<clr-icon shape="tag" class="icon-item" size="18"/>
 							{ " " + value }
 						</Tag>
 					)

e11bb8a66be0fd3f6387b79d5f931517d6eb751a
dantehemerson <dantehemerson@gmail.com>
2018-09-27 12:46:23 -0500

    Tag icon aligned

--- src/components/PortfolioItem/PortfolioItem.js
+++ src/components/PortfolioItem/PortfolioItem.js
@@ -90,6 +90,14 @@ const ItemLink = styled.article`
 
 const TagsWrapper = styled.div`
 	text-align: left;
+	.tag_icon {
+		width: 18px;
+		position: relative;
+		top: 4px;
+		path {
+			fill: #4E4E4E;
+		}
+	}
 `
 
 const Tag = styled(Link)`
@@ -102,7 +110,7 @@ const Tag = styled(Link)`
 	font-size: 13px;
 	font-weight: 600;
 	display: inline-block;
-	text-transform: uppercase;
+	text-transform: capitalize;
 	&:hover {
 		background: #E2E2E2;
 		// color: #BCBCBC;
@@ -134,7 +142,7 @@ export default props => (
 			<Title>{ props.data.title }</Title>
 		</ItemWrapper>
 		<TagsWrapper className="PortfolioItem__tagswrapper">
-			<svg className="icon_svg" version="1.1" width={36} height={36} viewBox="0 0 36 36" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+			<svg className="icon_svg tag_icon" version="1.1" width={36} height={36} viewBox="0 0 36 36" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
 			  <path d="M33.16,19.13,19.58,5.55A1.92,1.92,0,0,0,18.21,5H16.12L31.75,20.45,21.22,31.07a1.93,1.93,0,0,0,2.73,0l9.21-9.21a1.93,1.93,0,0,0,0-2.73Z" /><circle cx="7.81" cy="11.14" r="1.33" /><path d="M27.78,19.17,14.2,5.58A1.92,1.92,0,0,0,12.83,5H3.61A1.93,1.93,0,0,0,1.68,6.93v9.22a1.92,1.92,0,0,0,.57,1.36L15.84,31.1a1.93,1.93,0,0,0,2.73,0l9.21-9.21A1.93,1.93,0,0,0,27.78,19.17ZM17.26,29.69,3.69,16.15V7h9.1L26.37,20.48Z" />
 			  <rect x={0} y={0} width={36} height={36} fillOpacity={0} />
 			</svg>

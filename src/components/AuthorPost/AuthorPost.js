9791c437131bd16357b7a48ceb7510ddefbf46ba
dantehemerson <dantehemerson@gmail.com>
2018-09-28 13:06:36 -0500

    Change styles AuthorPost

--- src/components/AuthorPost/AuthorPost.js
+++ src/components/AuthorPost/AuthorPost.js
@@ -15,16 +15,16 @@ export default props => (
 				!props.header &&
 				<div className="Foot__AuthorPost__social">
 					<a href="https://www.github.com/dantehemerson">
-						<img alt="img" src="https://icongr.am/fontawesome/github.svg?color=ffffff&size=21" />
+						<img alt="img" src="https://icongr.am/fontawesome/github.svg?color=646464&size=21" />
 					</a>
 					<a href="https://twitter.com/dantehemerson">
-						<img alt="img" src="https://icongr.am/fontawesome/twitter.svg?color=ffffff&size=21" />
+						<img alt="img" src="https://icongr.am/fontawesome/twitter.svg?color=646464&size=21" />
 					</a>
 					<a href="https://www.linkedin.com/in/dantehemerson">
-						<img alt="img" src="https://icongr.am/fontawesome/linkedin.svg?color=ffffff&size=21" />
+						<img alt="img" src="https://icongr.am/fontawesome/linkedin.svg?color=646464&size=21" />
 					</a>
 					<a href="https://www.instagram.com/dantehemerson">
-						<img alt="img" src="https://icongr.am/fontawesome/instagram.svg?color=ffffff&size=21" />
+						<img alt="img" src="https://icongr.am/fontawesome/instagram.svg?color=646464&size=21" />
 					</a>
 				</div>
 			}

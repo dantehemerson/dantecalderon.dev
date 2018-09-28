4943b1b063457396c89aab20c0c455eb30a68f8b
dantehemerson <dantehemerson@gmail.com>
2018-09-28 14:24:10 -0500

    Changed style Author post footer

--- src/components/AuthorPostFooter/AuthorPostFooter.js
+++ src/components/AuthorPostFooter/AuthorPostFooter.js
@@ -10,8 +10,8 @@ export default props => (
 		<div className="AuthorPostFooter__body">
 			<p className="AuthorPostFooter__writtenby">Written by</p>
 			<Link to="/#about" className="AuthorPostFooter__name">Dante Calderón</Link>
-			<p className="AuthorPostFooter__about">Contribuidor de Software libre(@linux, @git), Desarrollador Web utilizando React, Node y Mongo. También me gusta desarrollar videojuegos, puedes ver los que he creado en mi <Link to="/portfolio">portafolio</Link>.</p>
-			<div className="Foot__AuthorPostFooter__social">
+			<p className="AuthorPostFooter__about">Contribuidor de Software libre(@linux, @git). Desarrollador Web utilizando React, Node y Mongo. También me gusta desarrollar videojuegos, puedes ver los que he creado en mi <Link to="/portfolio">portafolio</Link>.</p>
+			<div className="AuthorPostFooter__social">
 				<a href="https://www.github.com/dantehemerson">
 					<img alt="img" src="https://icongr.am/fontawesome/github.svg?color=646464&size=21" />
 				</a>

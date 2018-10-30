69e544046a6f5039f65f9f7fca298c5664a0f66c
dantehemerson <dantehemerson@gmail.com>
2018-10-30 08:42:30 -0500

    I know PHP2

--- src/components/Stack/Stack.js
+++ src/components/Stack/Stack.js
@@ -37,6 +37,7 @@ const groups = [
 			{ name: 'Javascript', icon: 'javascript-original.svg', background: '#f0db4f', type: type[1], about: 'Lightweight, interpreted, object-oriented language with first-class functions' },
 			{ name: 'Python', icon: 'python-original.svg', type: type[1], about: "Python is a clear and powerful object-oriented programming language, comparable to Perl, Ruby, Scheme, or Java."},
 			{ name: 'C++', icon: 'cplusplus-original.svg', type: type[1], about: "Has imperative, object-oriented and generic programming features, while also providing the facilities for low level memory manipulation"},
+			{ name: 'PHP', icon: 'php-original.svg', background: 'white', type: type[1], about: "A popular general-purpose scripting language that is especially suited to web development"},
 		]
 	},
 	{

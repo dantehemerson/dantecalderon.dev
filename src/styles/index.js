ac142c2f8a571d24c6bbb8bef2e3f44bcb42dfc7
dantehemerson <dantehemerson@gmail.com>
2018-11-01 15:26:38 -0500

    Add responsive utilities

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,6 +1,30 @@
 import { createGlobalStyle } from 'styled-components'
+import { css } from 'styled-components'
 
-const GlobalStyles = createGlobalStyle`
+const sizes = {
+  xl: 1170,
+  lg: 992,
+  md: 768,
+  sm: 576,
+}
+
+export const media = Object.keys(sizes).reduce((accumulator, label) => {  
+  accumulator[label] = (...args) => css`
+    @media (min-width: ${sizes[label]}px) {
+      ${css(...args)};
+    }
+  `
+  return accumulator
+}, {})
+
+export const defaultTheme = {
+  body: {
+    background: 'white',
+    color: 'black'
+  }
+}
+
+export const GlobalStyles = createGlobalStyle`
   body {
     margin: 0 !important;
     padding: 0 !important;
@@ -65,12 +89,3 @@ const GlobalStyles = createGlobalStyle`
       resize: vertical;
   }
 `
-
-const defaultTheme = {
-  body: {
-    background: 'white',
-    color: 'black'
-  }
-}
-
-export { GlobalStyles, defaultTheme }

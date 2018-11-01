0da7b703cf8a18fd623a8b853714ea1b9e70d682
dantehemerson <dantehemerson@gmail.com>
2018-11-01 14:09:33 -0500

    Remove transition for all elements

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,7 +1,18 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
-	
+  body {
+    margin: 0 !important;
+    padding: 0 !important;
+    text-rendering: geometricPrecision;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    color: #282a2d;
+  }
+  
+	textarea{
+      resize: vertical;
+  }
 `
 
 const defaultTheme = {

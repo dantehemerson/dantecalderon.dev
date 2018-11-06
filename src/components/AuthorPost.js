211d1a4dff5d40e17c3c32dbf8133e0e7861d1d9
dantehemerson <dantehemerson@gmail.com>
2018-11-06 13:27:43 -0500

    Author post to Styled-components

--- src/components/AuthorPost.js
+++ src/components/AuthorPost.js
@@ -1,14 +1,68 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Link } from 'gatsby'
 import Img from 'gatsby-image'
 
+import { media } from '../styles'
+
+const Container = styled.div`
+	display: flex;
+	margin-bottom: 13px;
+	padding-left: 0px;
+	margin-top: 20px;
+	font-family: 'Open Sans', sans-serif;
+`
+
+const Image = styled(Img)`
+	margin: 0;
+	width: 40px;
+	height: 40px;
+	border-radius: 50%;
+	margin-right: 13px;
+`
+
+const Body = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+`
+
+const Name = styled.p`
+	margin: 0;
+	font-size: 11px;
+	${media.sm`
+		font-size: 13px;
+		b {
+			display: inline-block !important;
+		}
+	`}
+	font-weight: 600;
+	color: #767676 !important;	
+	text-transform: uppercase;	
+	b {
+		color: #575757;
+		font-weight: 400;
+		display: none;
+	}
+`
+
+const Author = styled(Link)`
+	color: #0077b5 !important;
+	text-decoration: none !important;
+	display: block;
+	${media.sm`
+		display: inline-block;
+	`}
+`
+
 export default props => (
-	<div className='AuthorPost'>
-		<div className="AuthorPost__header">
-			<Img className="AuthorPost__image" sizes={ props.avatar.sizes } />
+	<Container>
+		<div>
+		<Image sizes={ props.avatar.sizes } />
 		</div>
-		<div className="AuthorPost__body">
-			<p className="AuthorPost__name"><Link to="/about">Dante Calderón</Link> <b>|</b> <span>{ props.date } · { props.timeToRead } min de lectura</span></p>
-		</div>
-	</div>
+		<Body>
+			<Name>
+				<Author to="/about">Dante Calderón</Author> <b>|</b> <span>{ props.date } · { props.timeToRead } min de lectura</span></Name>
+		</Body>
+	</Container>
 )
--- src/components/AuthorPostFooter.js
+++ src/components/AuthorPostFooter.js
@@ -1,30 +1,84 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Img from 'gatsby-image'
+import styled from 'styled-components'
 
+import { media } from '../styles'
+
+const Container = styled.div`
+	display: flex;
+	margin-bottom: 13px;
+	padding-left: 0px;
+	margin-top: 20px;
+	flex-direction: column;
+	align-items: center;
+	${media.md`
+		flex-direction: row;
+		align-items: inherit;
+	`}
+`
+
+const Image = styled(Img)`
+	margin: 0;
+	width: 90px;
+	height: 90px;
+	border-radius: 50%;
+	margin-right: 0;
+	${media.md`
+		width: 150px !important;
+		height: 150px !important;			
+		margin-right: 21px !important;
+	`}
+`
+const Body = styled.div`
+	display: flex;
+	font-family: 'Open Sans', sans-serif;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	padding-top: 14px;
+	${media.md`
+		align-items: inherit !important;
+		padding-top: 0 !important;
+	`}
+`
+const By = styled.p`
+	font-size: 14px;
+	text-transform: uppercase;
+	font-weight: 700;
+	color: #767676;
+	margin: 0;
+`
+
+const Name = styled(Link)`
+	margin: 0;
+	font-size: 26px;
+	line-height: 1;
+	font-weight: 700;		
+	color: #292929 !important;
+	text-decoration: none !important;
+`
+
+const About = styled.p`
+	color: rgba(0,0,0,.54) !important;
+	font-size: 15px;
+	font-weight: 600;
+	padding-top: 10px;
+	margin: 0;
+	text-align: center;
+	${media.md`
+		text-align: left !important;
+	`}
+`
 export default props => (
-	<div className='AuthorPostFooter'>
-		<div className="AuthorPostFooter__header">
-			<Img className="AuthorPostFooter__image" sizes={ props.avatar.sizes } />
+	<Container>
+		<div>
+			<Image sizes={ props.avatar.sizes } />
 		</div>
-		<div className="AuthorPostFooter__body">
-			<p className="AuthorPostFooter__writtenby">{ props.make ? 'Hecho' : 'Escrito'} por</p>
-			<Link to="/#about" className="AuthorPostFooter__name">Dante Calderón</Link>
-			<p className="AuthorPostFooter__about">Contribuidor de Software libre(@linux, @git). Desarrollador Web utilizando React, Node y Mongo. También me gusta desarrollar videojuegos, puedes ver los que he creado en mi <Link to="/portfolio">portafolio</Link>.</p>
-			<div className="AuthorPostFooter__social">
-				<a href="https://www.github.com/dantehemerson">
-					<img alt="img" src="https://icongr.am/fontawesome/github.svg?color=646464&size=21" />
-				</a>
-				<a href="https://twitter.com/dantehemerson">
-					<img alt="img" src="https://icongr.am/fontawesome/twitter.svg?color=646464&size=21" />
-				</a>
-				<a href="https://www.linkedin.com/in/dantehemerson">
-					<img alt="img" src="https://icongr.am/fontawesome/linkedin.svg?color=646464&size=21" />
-				</a>
-				<a href="https://www.instagram.com/dantehemerson">
-					<img alt="img" src="https://icongr.am/fontawesome/instagram.svg?color=646464&size=21" />
-				</a>
-			</div>
-		</div>
-	</div>
+		<Body>
+			<By>{ props.make ? 'Hecho' : 'Escrito'} por</By>
+			<Name to="/about">Dante Calderón</Name>
+			<About>Contribuidor de Software libre(@linux, @git). Desarrollador Web utilizando React, Node y Mongo. También me gusta desarrollar videojuegos, puedes ver los que he creado en mi <Link to="/portfolio">portafolio</Link>.</About>			
+		</Body>
+	</Container>
 )

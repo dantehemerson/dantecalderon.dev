11acfb08769cd08627a1e639278aeaa3a5c45f5e
dantehemerson <dantehemerson@gmail.com>
2018-11-08 15:58:54 -0500

    Change style 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,62 +1,52 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import Helmet from 'react-helmet'
 import styled from 'styled-components'
+import { AwesomeButton } from 'react-awesome-button'
 
-import Layout from '../components/Layout'
+import image from '../assets/images/404.jpg'
+import { media } from '../styles'
 
 const Container = styled.main`
-	display: flex;
-	background: #fbf8f3;
+	display: flex;	
 	justify-content: center;
 	flex-direction: column;
 	align-items: center;
 	height: 100vh;
 	min-height: 380px;
-
-	svg{
-		font-weight:bold;
-		max-width:600px;
-		height:auto;
-	}
 `
+
 const Title = styled.h1`
-	font-family: 'Open Sans';
 	text-align: center;
-	font-size: 5vw;
+	font-size: 16px;	
+	padding: 25px 10px;
+	font-weight: 400;
+	text-transform: uppercase;
+	font-family: sans-serif !important;
+	margin: 0;
+	color: #4B6A8E;
+	${media.sm`
+		font-size: 20px;
+	`}
 `
 
-const Description = styled.p`
-	text-align: center;
-	color: #989898;
-	a {
-		color: #282a2d;
-	}
+const Image = styled.img`
+	width: 100%;
+	max-width: 479px;
 `
 
-export default props => (
-		<Layout location={ props.location }>
-			<Container>
-				<Helmet>
-					<title>{`Ops :(`}</title>
-				</Helmet>
-				<svg viewBox="0 0 100 33">
-					<defs>
-						<linearGradient id="gradient" x1={0} x2={0} y1={0} y2={1}>
-							<stop offset="5%" stopColor="#CB9A48" />
-							<stop offset="95%" stopColor="#C89B4F" />
-						</linearGradient>
-						<pattern id="wave" x={0} y={11} width={120} height={35} patternUnits="userSpaceOnUse">
-							<path id="wavePath" d="M-40 9 Q-30 7 -20 9 T0 9 T20 9 T40 9 T60 9 T80 9 T100 9 T120 9 V20 H-40z" mask="url(#mask)" fill="url(#gradient)">
-								<animateTransform attributeName="transform" begin="0s" dur="1.5s" type="translate" from="0,0" to="40,0" repeatCount="indefinite" />
-							</path>
-						</pattern>
-					</defs>
-					<text textAnchor="middle" x={50} y={30} fontSize={30} fill="url(#wave)" fillOpacity="0.6">404</text>
-					<text textAnchor="middle" x={50} y={30} fontSize={30} fill="url(#gradient)" fillOpacity="0.1">404</text>
-				</svg>
-				<Title>Página no encontrada</Title>
-				<Description>Ir al <Link to="/">Inicio</Link>.</Description>
-			</Container>
-		</Layout>
+const Button = styled(AwesomeButton)`
+
+`
+
+export default props => (		
+	<Container>
+		<Helmet>
+			<title>{`Ops :(`}</title>
+		</Helmet>
+		<Image src={image}/>
+		<Title>No encontramos lo que buscabas</Title>		
+		<Button
+			href='/'
+			>VOLVER AL INICIO</Button>
+	</Container>		
 )

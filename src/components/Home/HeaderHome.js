feb2c97b22823b6cfb7bee8deac3e1ac0bf8f028
dantehemerson <dantehemerson@gmail.com>
2018-11-17 11:41:39 -0500

    improve headerhome

--- src/components/Home/HeaderHome.js
+++ src/components/Home/HeaderHome.js
@@ -5,33 +5,61 @@ import styled from 'styled-components'
 
 import Social from '../Social'
 import Rotational from '../Rotational'
+import Wrapper from '../Wrapper'
+import { media } from '../../styles'
+import grain_image from '../../assets/images/grain.png'
 
 const ButtonAbout = styled(AwesomeButton)`
   width: 141px !important;
   margin-top: 34px !important;
 `
 
+const Container = styled(Wrapper)`
+  min-height: 600px;
+  padding-top: 100px;
+  background-color: #f3f3f3;
+  background-image: url(${grain_image});
+  ${media.lg`
+    height: 100vh;
+    min-height: 500px;
+    padding-top: 38px;
+  `}
+`
+
+const Title = styled.h1`
+  font-size: 12vw;
+  font-weight: 900;
+  text-transform: uppercase;
+  text-align: center;
+  color: #363636;
+  ${media.sm`
+    font-size: 4rem;
+  `}
+`
+
+const Subtitle = styled.h2`
+  font-size: 18px;
+  font-weight: 600;
+  margin: 10px 0 20px 0;
+  font-family: 'Open Sans', sans-serif;
+  text-align: center;
+  color: #1976d2;
+`
+
 const HeaderHome = ({ data }) => (
-  <main className="Header-Home">
-    <div className="container">
-      <div className="row">
-        <div className="col-xs-12 col-lg-5" style={{
-          textAlign: 'center'
-        }} >
-          <h1 className="Header-Home__title" >{ data.site.siteMetadata.title }</h1>
-          <h2 className="Header-Home__subtitle">{ data.site.siteMetadata.subtitle }</h2>
-          <Social />
-          <ButtonAbout
-            href="/about" >
-            SOBRE MÍ
-          </ButtonAbout>
-        </div>
-        <div className="col-xs-12 col-lg-7">
-          <Rotational avatar={ data.avatar }/>
-        </div>
+  <Container
+    className='row'
+    justifyContent='center'>
+      <div className="col-xs-12 col-lg-5 center-xs">
+        <Title>{ data.site.siteMetadata.title }</Title>
+        <Subtitle>{ data.site.siteMetadata.subtitle }</Subtitle>
+        <Social/>
+        <ButtonAbout href="/about" >SOBRE MÍ</ButtonAbout>
       </div>
-    </div>
-  </main>
+      <div className="col-xs-12 col-lg-7">
+        <Rotational avatar={ data.avatar }/>
+      </div>
+  </Container>
 )
 
 export default props => (

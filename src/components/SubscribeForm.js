3c6e545c5e77f9c4ae8aaf3744747c4702d9b43a
Dante Calderon <dantehemerson@gmail.com>
2019-07-26 21:51:06 -0500

    Improve styles and add dotenv cofig for local development

--- src/components/SubscribeForm.js
+++ src/components/SubscribeForm.js
@@ -1,6 +1,41 @@
 import React, { useState } from 'react'
 import addToMailchimp from 'gatsby-plugin-mailchimp'
+import styled from 'styled-components'
 
+const Form = styled.form`
+  background: #97d1ff;
+  margin: 10px auto;
+  align-items: center;
+  padding: 14px;
+  opacity: 0.9;
+  display: flex;
+  flex-direction: column;
+  border: 7px solid #2e87e7;
+  max-width: 600px;
+`
+const Title = styled.h2`
+  margin-bottom: 1rem;
+`
+const Input = styled.input`
+  padding: 10px 1rem !important;
+  border-color: #939393 !important;
+`
+const Button = styled.button`
+  margin: 15px 0;
+  margin-top: 26px;
+  background: #fad257;
+  border: 0;
+  border-radius: 3px;
+  padding: 10px 32px;
+  transition: 0.2s;
+  cursor: pointer;
+  font-weight: bold;
+  box-shadow: 0 0 2px 0px gray;
+  &:hover {
+    background: #fad257;
+    box-shadow: 0 0 4px 0px gray;
+  }
+`
 const SubscribeForm = () => {
   const [email, setEmail] = useState('')
 
@@ -8,7 +43,8 @@ const SubscribeForm = () => {
     e.preventDefault()
     addToMailchimp(email)
       .then(data => {
-        alert(data.result)
+        console.log(data)
+        alert(data.result + '  ' + data.msg)
       })
       .catch(error => {
         // Errors in here are client side
@@ -21,13 +57,11 @@ const SubscribeForm = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Subscribe to my email list!</h2>
-      <div>
-        <input placeholder="Email address" name="email" type="text" onChange={handleEmailChange} />
-        <button type="submit">Subscribe</button>
-      </div>
-    </form>
+    <Form onSubmit={handleSubmit}>
+      <Title>Subscribe to my email list!</Title>
+      <Input placeholder="Email address" name="email" type="text" onChange={handleEmailChange} />
+      <Button type="submit">Subscribe</Button>
+    </Form>
   )
 }
 

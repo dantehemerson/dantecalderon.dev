25855ec378ee2640781de8c3dfaf4375365cfc65
Dante Calderon <dantehemerson@gmail.com>
2019-07-28 17:18:03 -0500

    Separe some data

---
+++ about.js
@@ -0,0 +1,35 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Dante Calderón',
+    subtitle: 'Javascript / Node.js Developer',
+    description: `Hi, I'm Dante Calderón, I am passionate about Nodejs.`,
+    siteUrl: 'https://dantecalderon.dev',
+    disqusShortname: 'dantecalderon',
+    social: [
+      { title: 'Github', link: 'https://github.com/dantehemerson', icon: 'github' },
+      { title: 'Twitter', link: 'https://twitter.com/dantehemerson', icon: 'twitter' },
+      { title: 'Linkedin', link: 'https://linkedin.com/in/dantehemerson', icon: 'linkedin' },
+      { title: 'Medium', link: 'https://medium.com/@dantehemerson', icon: 'medium' },
+      //{ title: 'Dev', link: 'https://dev.to/dantehemerson',  icon: 'dev' },
+      { title: 'Dev', link: 'https://instagram.com/dantehemerson', icon: 'dev' },
+      { title: 'StackOverflow', link: 'https://instagram.com/dantehemerson', icon: 'stack-overflow' },
+      { title: 'Goodreads', link: 'https://instagram.com/dantehemerson', icon: 'instagram' },
+      { title: 'FreeCodeCamp', link: 'https://instagram.com/dantehemerson', icon: 'free-code-camp' },
+      { title: 'Gitlab', link: 'https://instagram.com/dantehemerson', icon: 'gitlab' },
+      { title: 'Angellist', link: 'https://angel.co/dantehemerson', icon: 'angellist' },
+      { title: 'Codepen', link: 'https://codepen.io/dantehemerson', icon: 'codepen' },
+      { title: 'Meetup', link: 'https://www.meetup.com/es/members/256853185/', icon: 'meetup' },
+      { title: 'Pinterest', link: 'https://www.pinterest.com/dantehemerson/', icon: 'pinterest' },
+      { title: 'Steam', link: 'https://steamcommunity.com/id/dantehemerson/', icon: 'steam' },
+      { title: 'Twitch', link: 'https://www.twitch.tv/dantehemerson', icon: 'twitch' },
+      { title: 'Youtube', link: 'https://www.youtube.com/channel/UCCbtGSRgh1RFYysgaZkoE0A', icon: 'youtube-play' }
+    ],
+    menu: [
+      { title: 'Home', id: '', to: '/', icon: 'home' },
+      { title: 'Blog', id: 'Blog', to: '/blog', icon: 'home' },
+      { title: 'About', id: 'About', to: '/about', icon: 'home' },
+      { title: 'Portfolio', id: 'Portfolio', to: '/portfolio', icon: 'home' }
+      //{ title: 'Contact', id: 'Contact', to: '/contact', icon: 'home' }
+    ]
+  }
+}

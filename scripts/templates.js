62e40086724576773e6ebbe5e02429ef43c5c720
Dante Calderon <mail@dantecalderon.dev>
2022-01-23 16:10:22 -0500

    Do not add field if param does not exist

--- scripts/templates.js
+++ scripts/templates.js
@@ -5,7 +5,9 @@ description: {{ description }}
 author: Dante Calderón
 pathPrefix: blog
 slug: {{ slug }}
+{{#image}}
 image: {{ image }}
+{{/image}}
 externalImage: {{{ externalImage }}}
 date: '{{ createdAt }}'
 published: {{ published }}

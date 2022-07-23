b26f88d89ba527693649c116d3dc4ffe1802453f
Dante Calderon <mail@dantecalderon.dev>
2022-07-23 17:12:40 -0500

    Styling ?

--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -3,11 +3,6 @@ import { Link } from 'gatsby'
 import styled from 'styled-components'
 import { generateTagInfo } from '../helpers/tags.helpers'
 
-const List = styled('ul')`
-  list-style: none;
-  margin: 0;
-`
-
 const Container = styled('div')`
   width: 100%;
   margin-left: auto;
@@ -19,24 +14,32 @@ const Container = styled('div')`
   }
 `
 
-const Item = styled(props => <Link {...props} />)`
-  padding: 2px 7px;
-  background: #f7f7f7;
-  color: #6a6a6a;
-  border-radius: 3px;
-  font-size: 13px;
-  font-family: 'Open Sans', sans-serif;
-  font-weight: 600;
-  min-width: 32px;
-  text-align: center;
-  display: inline-block;
-  text-transform: capitalize;
-  text-decoration: none !important;
-  transition: 0.2s;
-  transition-timing-function: ease-in;
-  margin-right: 4px !important;
-  &:hover {
-    opacity: 0.88;
+const List = styled('ul')`
+  list-style: none;
+  display: flex;
+  margin: 0;
+`
+
+const Item = styled('li')`
+  .tagLink {
+    padding: 2px 7px;
+    background: #f7f7f7;
+    color: #6a6a6a;
+    border-radius: 3px;
+    font-size: 13px;
+    font-family: 'Open Sans', sans-serif;
+    font-weight: 600;
+    min-width: 32px;
+    text-align: center;
+    display: inline-block;
+    text-transform: capitalize;
+    text-decoration: none !important;
+    transition: 0.2s;
+    transition-timing-function: ease-in;
+    margin-right: 4px !important;
+    &:hover {
+      opacity: 0.88;
+    }
   }
 `
 
@@ -44,21 +47,24 @@ export function Tags({ items = [] }: { items: string[] }) {
   return (
     <Container>
       <List>
-        {/* {items.map(item => {
+        {items.map(item => {
           const tagData = generateTagInfo(item)
           return (
-            <Item
-              key={tagData.slug}
-              to={`/blog/tags/${tagData.slug}`}
-              style={{
-                backgroundColor: tagData.color,
-                color: tagData.textColor,
-              }}
-            >
-              {tagData.title}
+            <Item>
+              <Link
+                className="tagLink"
+                key={tagData.slug}
+                to={`/blog/tags/${tagData.slug}`}
+                style={{
+                  backgroundColor: tagData.color,
+                  color: tagData.textColor,
+                }}
+              >
+                {tagData.title}
+              </Link>
             </Item>
           )
-        })} */}
+        })}
       </List>
     </Container>
   )

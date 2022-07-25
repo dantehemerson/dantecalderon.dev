1967604d74d761a197a8ef59160185805bff84ab
Dante Calderon <mail@dantecalderon.dev>
2022-07-24 21:29:21 -0500

    Fix

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,5 +1,5 @@
+import React, { useEffect } from 'react'
 import { mergeAdvanced } from 'object-merge-advanced'
-import { useEffect } from 'react'
 import styled from 'styled-components'
 import { GLOBAL_CONTEXT_KEY } from '../../gatsby/gatsby.constants'
 import { secureTimeAgo } from '../helpers/date'
